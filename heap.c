a61fa5e7208ee8bd66cebd5c70201ca54484376c
Replit user <>
2023-05-11 16:37:14 +0000

    new try

--- heap.c
+++ heap.c
@@ -48,17 +48,19 @@ void arreglarArriba(Heap* pq, int i){
 
 void heap_push(Heap* pq, void* data, int priority){
 
-  
-
-  (*(pq->heapArray+pq->size)).priority = priority;
-  (*(pq->heapArray+pq->size)).data = data;
-
-  arreglarArriba(pq, pq->size);
-  pq->size++;
   if(pq->size == pq->capac){
     pq->capac = pq->capac*2 + 1; 
     pq->heapArray = realloc(pq->heapArray, pq->capac );
   }
+  pq->size++;
+
+  int i = pq->size -1;
+  
+  (*(pq->heapArray+i)).priority = priority;
+  (*(pq->heapArray+i)).data = data;
+
+  arreglarArriba(pq, pq->size);
+  
 }
 
 
